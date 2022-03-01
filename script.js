b1311e2b72efeb3a41b06727e30215cf0133bc4d
Piero <dolibiroli@gmail.com>
2022-03-01 10:43:50 -0600

    Fixed reset button, now completely removes old divs instead of adding more and removing hover class

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
 function createGrid(grid = 16) {
     const container = document.querySelector('.container');
+    const widthBox = Math.floor(1000/grid);
+    console.log(widthBox);
     for (let i = 0; i < grid * grid; i++) {
         const div = document.createElement('div');
         container.appendChild(div);
-        div.classList.add('square')
+        div.classList.add('square');
+        div.style.width = `${widthBox}px`;
+        div.style.height = `${widthBox}px`;
         div.addEventListener('mouseover',() => div.classList.add('hover'));
     }
 }
@@ -11,7 +15,7 @@ function createGrid(grid = 16) {
 const button = document.querySelector('#reset');
 button.addEventListener('click', () => {
     const boxes = document.querySelectorAll(".container div");
-    boxes.forEach((box) => box.classList.remove('hover'));
+    boxes.forEach((box) => box.remove());
     let gridSize = prompt('Enter a grid size (max 100)');
     createGrid(gridSize);
 })
