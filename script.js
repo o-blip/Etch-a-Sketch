d18bd3769801258755d1799b110660a011a4b022
Piero <dolibiroli@gmail.com>
2022-03-01 22:36:26 -0600

    made squares fill with random colors

--- script.js
+++ script.js
@@ -1,22 +1,51 @@
+createGrid(); // create initial grid
+addHover(); // create initial hover effect
+
+// reset grid and create new grid
+const button = document.querySelector('#reset');
+button.addEventListener('click', () => {
+    let gridSize = prompt('Enter a grid size (max 100)');
+    console.log(gridSize);
+    createGrid(gridSize);
+    addHover();
+})
+
+//
+function addHover() {
+    const squares = document.querySelectorAll('.square');
+    squares.forEach((square) => {
+        square.addEventListener('mouseover', () => {
+            // need to skip filled squares, and darken them instead of replace the color with
+            // another random color. <if background-color not empty => darken shade, 
+            //                          else background-color = randomColor>
+            if (square.style.backgroundColor === '') {
+                const randomColor = Math.floor(Math.random() * 16777215).toString(16);
+                square.style.backgroundColor = '#' + randomColor;
+
+            }
+        });
+    });
+
+}
+
+
+
+
+
 function createGrid(grid = 16) {
+    if (isNaN(grid)) {
+        grid = 16;
+    }
+    const boxes = document.querySelectorAll(".container div");
+    boxes.forEach((box) => box.remove());
     const container = document.querySelector('.container');
-    const widthBox = 1000/grid; // problem with decimal pixels maybe?
-    console.log(widthBox);
+    const widthBox = 1000 / grid; // problem with decimal pixels maybe?
     for (let i = 0; i < grid * grid; i++) {
         const div = document.createElement('div');
         container.appendChild(div);
         div.classList.add('square');
         div.style.width = `${widthBox}px`;
         div.style.height = `${widthBox}px`;
-        div.addEventListener('mouseover',() => div.classList.add('hover'));
     }
 }
 
-createGrid();
-const button = document.querySelector('#reset');
-button.addEventListener('click', () => {
-    const boxes = document.querySelectorAll(".container div");
-    boxes.forEach((box) => box.remove() );
-    let gridSize = prompt('Enter a grid size (max 100)');
-    createGrid(gridSize);
-})
